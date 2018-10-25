2311e283da2a8550468fe093105353147a24d98f
Myo T Kyaw <myotkyaw@gmail.com>
2018-10-25 16:24:50 -0700

    Update routes; Update Cars controller to be able to search for cars that belong to the person

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,9 +1,22 @@
 class CarsController < ApplicationController
+  before_action :set_person
+  before_action :set_car
+
   def index
-    render json: Cars.all
+    render json: @person.cars
   end
 
   def show
-    render json: Car.find(params[:id])
+    render json: @car
+  end
+
+  private
+
+  def set_person
+    @person = Person.find(params[:person_id])
+  end
+
+  def set_car
+    @car = @person.cars.find_by(id: params[:id])
   end
 end
