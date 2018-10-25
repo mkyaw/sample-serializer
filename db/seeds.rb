53c3c924abc48808cda9aee2a6605e483c849ee7
Myo T Kyaw <myotkyaw@gmail.com>
2018-10-25 16:00:37 -0700

    Add DB seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Person.destroy_all
+Car.destroy_all
+
+pee = Person.create({ name: 'Pee', age: 28 })
+nee = Person.create({ name: 'Nee', age: 27 })
+
+pee.cars.create({ name: 'koolie', make: 'Honda', model: 'Accord' })
+pee.cars.create({ name: 'rookie', make: 'Toyota', model: 'Camry' })
+
+nee.cars.create({ name: 'hottie', make: 'Mini Cooper', model: 'Coupe S' })
+nee.cars.create({ name: 'beau', make: 'Mazda', model: 'Mazda 3' })
