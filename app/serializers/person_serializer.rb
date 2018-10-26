455ec575e9edbcb47910be37d114664c2b444be3
Myo T Kyaw <myotkyaw@gmail.com>
2018-10-25 17:28:01 -0700

    Add  attribute to person with its own method defining which fields to show in the response

--- app/serializers/person_serializer.rb
+++ app/serializers/person_serializer.rb
@@ -1,3 +1,13 @@
 class PersonSerializer < ActiveModel::Serializer
-  attributes :id, :name, :age
+  attributes :id, :name, :age, :cars
+
+  def cars
+    self.object.cars.map do |car|
+      {
+        name: car.name,
+        make: car.make,
+        model: car.model
+      }
+    end
+  end
 end
