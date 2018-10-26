e32d0c24c1e158674aba4ba94dd8131af6191885
Myo T Kyaw <myotkyaw@gmail.com>
2018-10-25 17:14:26 -0700

    Refactor Cars controller for the POC of serializer

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,22 +1,9 @@
 class CarsController < ApplicationController
-  before_action :set_person
-  before_action :set_car
-
   def index
-    render json: @person.cars
+    render json: Car.all
   end
 
   def show
-    render json: @car
-  end
-
-  private
-
-  def set_person
-    @person = Person.find(params[:person_id])
-  end
-
-  def set_car
-    @car = @person.cars.find_by(id: params[:id])
+    render json: Car.find(id: params[:id])
   end
 end
