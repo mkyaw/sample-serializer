938b675b86f79352b4a5d3b94c9d6619fdbcc471
Myo T Kyaw <myotkyaw@gmail.com>
2018-10-25 17:27:26 -0700

    Add custom owner attribute to car with its own method

--- app/serializers/car_serializer.rb
+++ app/serializers/car_serializer.rb
@@ -1,3 +1,11 @@
 class CarSerializer < ActiveModel::Serializer
-  attributes :name, :make, :model
+  attributes :name, :make, :model, :owner
+
+  def owner
+    {
+      owner_id: self.object.person.id,
+      owner_name: self.object.person.name,
+      owner_age: self.object.person.age
+    }
+  end
 end
