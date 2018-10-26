422e03046c0e134b718f1b485ea91018b5bf3848
Myo T Kyaw <myotkyaw@gmail.com>
2018-10-25 17:14:54 -0700

    Refactor route file for POC

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :people, only: [:index, :show] do
-    resources :cars, only: [:index, :show]
-  end
+  resources :people, only: [:index, :show]
+  resources :cars, only: [:index, :show]
 end
